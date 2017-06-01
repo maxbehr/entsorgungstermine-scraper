522471ba425b22162377b2267fbdb9e695415cd8
max <hello@maxbehr.de>
2017-06-01 22:41:06 +0200

    Initial commit
    Add scraper and data cleanup script for potsdam.abfallkalender.de

---
+++ EntsorgungsDataCleaner.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+import os, os.path
+import json
+import io
+from pprint import pprint
+import fileinput
+import re
+
+path = '<Path>'
+path_summary = '<Path>'
+
+
+month_mapping = { "btMCMonth1":"Januar", "btMCMonth2":"Februar", "btMCMonth3":"März", "btMCMonth4":"April", "btMCMonth5":"Mai", "btMCMonth6":"Juni", "btMCMonth7":"Juli", "btMCMonth8":"August", "btMCMonth9":"September", "btMCMonth10":"Oktober", "btMCMonth11":"November", "btMCMonth12":"Dezember" }
+
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    '''
+    return [ atoi(c) for c in re.split('(\d+)', text) ]
+
+def main():
+    files = [name for name in os.listdir(path) if os.path.isfile(os.path.join(path, name)) and name != '.DS_Store']
+    files = map(lambda n: unicode(n, 'utf-8'), files)
+    files.sort(key=lambda x: get_streetname(x))
+
+    print "Anzahl Straßen: " + str(len(files))
+
+    all_streets = []
+    print "Hole Straßeninformationen..."
+    filename = files[0]
+    #for filename in files:
+    f = path + filename
+
+    with io.open(f, encoding='utf-8') as data_file:
+        street_id = filename.split(' ')[0]
+        street_name = ' '.join(filename.split('.')[0].split(' ')[1:])
+
+        # print street_name
+
+        street = {
+            "id": street_id,
+            "name": street_name # street_name.decode('cp1250')
+        }
+
+        data = json.load(data_file)
+        months = data.itervalues().next()
+
+        # Sort months in human sorting
+        m = []
+        for key in months.keys():
+            m.append(key);
+
+        month_values = []
+        m.sort(key=natural_keys)
+        for month_label in m:
+            month_name = month_mapping[month_label]
+            print month_name
+
+            month_obj = {
+                "month": month_name,
+                "values": months[month_label]
+            }
+
+            month_values.append(month_obj)
+
+        print month_values
+
+        street['months'] = month_values
+
+        all_streets.append(street)
+        data_file.close()
+
+    # Write to summary file
+    print "Schreibe Gesamtübersicht..."
+    write_summary(all_streets)
+
+def get_streetname(str):
+    return ' '.join(str.split('.')[0].split(' ')[1:])
+
+def write_summary(all_streets_arr):
+    sum_file = path_summary + 'entsorgungstermine.json'
+    if os.path.exists(sum_file):
+        os.remove(sum_file)
+
+    with open(sum_file, 'w') as outfile:
+        json.dump(all_streets_arr, outfile)
+
+if __name__ == '__main__':
+    main()
---
+++ EntsorgungsScraperSelenium.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+
+from lxml import html
+import requests
+import json
+import datetime
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import StaleElementReferenceException
+import os
+import sys
+
+
+url = 'https://potsdam.abfuhrkalender.de/default.aspx'
+months_btn_ids = ['btMCMonth1', 'btMCMonth2', 'btMCMonth3', 'btMCMonth4', 'btMCMonth5', 'btMCMonth6', 'btMCMonth7', 'btMCMonth8', 'btMCMonth9', 'btMCMonth10', 'btMCMonth11', 'btMCMonth12']
+
+PATH = "<PATH>"
+
+
+start_index = 0
+amount_streets = 1042
+time_to_wait = 1
+
+current_year = datetime.datetime.now().year
+
+def scrape(start_index, last_index):
+    try:
+        global driver
+        driver = webdriver.Firefox()
+        driver.set_window_size(640, 480)
+
+        print "Start scraping with street_index " + str(start_index) + ", stop at index " + str(last_index) + " (inclusive)"
+
+        disposals_all = []
+
+        start_index = int(start_index)
+        last_index = int(last_index) + 1
+
+        street_index_pointer = -1
+        for street_index in range(start_index, last_index):
+            street_index_pointer = street_index
+
+            driver.get(url)
+            assert "Abfuhrkalender der Landeshauptstadt Potsdam" in driver.title
+
+            # Change Ortsteil
+            element = driver.find_element_by_xpath('//*[@id="ddOrtsteil"]')
+            options = element.find_elements_by_tag_name('option')
+
+            for option in options:
+                if option.get_attribute('value') == '-1':
+                    option.click()
+                    break
+
+            # Wait until drop down is enabled
+            wait = WebDriverWait(driver, 10)
+            element = wait.until(EC.element_to_be_clickable((By.ID,'ddStrasse')))
+            option = element.find_element_by_xpath('//*[@id="ddStrasse"]/option[@value="'+ str(street_index) +'"]')
+            street_name = option.text
+            option.click()
+            #amount_streets = len(options) - 1 # minus 1 because one empty option inside the select
+            #print str(amount_streets) + " Streets"
+
+            disposals = {}
+            disposals[street_name]= {}
+
+            #
+            #   Loop through months of <STREET>
+            #
+            start = time.time()
+            print street_name + ":"
+            for month_id in range(1, 13):
+                month = "btMCMonth" + str(month_id)
+
+                disposals[street_name][month] = {}
+
+                wait = WebDriverWait(driver, 10)
+                element = wait.until(EC.element_to_be_clickable((By.ID, month)))
+                element.click()
+
+                print "\t...scraping " + month
+
+                waiting()
+                rows = driver.find_elements_by_xpath('//*[@id="pnlMonthCalendarDays"]/div[@class="RowStandard"]')
+
+                arr = []
+                for row in rows:
+                    wd = row.find_element_by_class_name('Date1').text.encode('utf-8').strip()
+                    date = row.find_element_by_class_name('Date2').text.encode('utf-8').strip()
+                    descr = row.find_element_by_class_name('SymbolDescription').text.encode('utf-8').strip()
+
+                    if descr != '':
+                        year = current_year+1 if month_id < datetime.datetime.now().month else current_year
+
+                        dic = {
+                            "date": str(datetime.date(year, month_id, int(date))),
+                            "wd": wd,
+                            "descr": descr
+                        }
+
+                        arr.append(dic)
+
+                disposals[street_name][month] = arr
+                end = time.time()
+
+            measured_time = (end - start)
+            print "Finished in " +  street_name + " (" + str(street_index) + ") in " + str(measured_time) + "s"
+
+            # Put into all disposals array
+            disposals_all.append(disposals)
+
+            # Write to single street file
+            with open(PATH + str(street_index) + " " + street_name +'.txt', 'w') as outfile:
+                json.dump(disposals, outfile)
+
+            # Choose a new street
+            driver.get(url)
+
+        # Close session
+        driver.close()
+        driver.quit()
+
+        # Write to summary file
+        #with open(PATH + 'entsorgungstermine.txt', 'w') as outfile:
+        #    json.dump(disposals_all, outfile)
+    except StaleElementReferenceException:
+        print "StaleElementReferenceException... start over with index " + str(street_index_pointer) + " as start_index and " + str(last_index) + " as last_index"
+        scrape(street_index_pointer, last_index)
+
+
+def waiting():
+    time.sleep(time_to_wait)
+    driver.implicitly_wait(time_to_wait)  # seconds
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        scrape(sys.argv[1], sys.argv[2])
+    else:
+        scrape(1, 10)
+
+
+
+
