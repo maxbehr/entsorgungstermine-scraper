8a1eb49697df8d367af97e09ec50c338b6614512
max <hello@maxbehr.de>
2017-10-30 22:41:33 +0100

    Loop through all files

--- EntsorgungsDataCleaner.py
+++ EntsorgungsDataCleaner.py
@@ -35,60 +35,65 @@ def main():
     all_streets = []
     filename = files[0]
     print "Hole Straßeninformationen für " + str(filename)
-    #for filename in files:
-    f = path + filename
+    for filename in files:
+        f = path + filename
 
-    with io.open(f, encoding='utf-8') as data_file:
-        street_id = filename.split(' ')[0]
-        street_name = ' '.join(filename.split('.')[0].split(' ')[1:])
+        with io.open(f, encoding='utf-8') as data_file:
+            street_id = filename.split(' ')[0]
+            street_name = ' '.join(filename.split('.')[0].split(' ')[1:])
 
-        # print street_name
+            # print street_name
 
-        street = {
-            "id": street_id,
-            "name": street_name # street_name.decode('cp1250')
-        }
-
-        data = json.load(data_file)
-        months = data.itervalues().next()
-
-        # Sort months in human sorting
-        m = []
-        for key in months.keys():
-            m.append(key);
-
-        month_values = []
-        m.sort(key=natural_keys)
-        for month_label in m:
-            month_name = month_mapping[month_label]
-            print month_name
-
-            print months[month_label]
-
-            # Create descriptions
-            months[month_label] = map(lambda day: dayMapping(day), months[month_label])
-
-            print '\n'
-
-            month_obj = {
-                "month": month_name,
-                "values": months[month_label]
+            street = {
+                "id": street_id,
+                "name": street_name # street_name.decode('cp1250')
             }
 
-            month_values.append(month_obj)
+            data = json.load(data_file)
+            months = data.itervalues().next()
 
-        street['months'] = month_values
+            # Sort months in human sorting
+            m = []
+            for key in months.keys():
+                m.append(key);
 
-        all_streets.append(street)
-        data_file.close()
+            month_values = []
+            m.sort(key=natural_keys)
+            for month_label in m:
+                month_name = month_mapping[month_label]
+                print month_name
 
-    # Write to summary file
-    print "Schreibe Gesamtübersicht..."
-    write_summary(street)
+                print months[month_label]
+
+                # Create descriptions
+                months[month_label] = map(lambda day: dayMapping(day), months[month_label])
+
+                print '\n'
+
+                month_obj = {
+                    "month": month_name,
+                    "values": months[month_label]
+                }
+
+                if len(months[month_label]) > 0:
+                    month_obj["year"] = months[month_label][0]["date"]
+
+                month_values.append(month_obj)
+
+            street['months'] = month_values
+
+            all_streets.append(street)
+            data_file.close()
+
+            # Write to summary file
+            print "Schreibe Gesamtübersicht für " + str(street_name)
+            file_name = str(street_id) + " " + str(street_name)
+            write_summary(file_name, street)
 
 def dayMapping(day):
     d = {
         "date": day['date'],
+        "type": day['type'],
         "descr": createDescriptionArray(day['descr'])
     }
     return d
@@ -100,13 +105,13 @@ def createDescriptionArray(descrStr):
 def get_streetname(str):
     return ' '.join(str.split('.')[0].split(' ')[1:])
 
-def write_summary(all_streets_arr):
-    sum_file = path_summary + 'entsorgungstermine.json'
+def write_summary(street_name, street_data):
+    sum_file = path_summary + street_name + '.json'
     if os.path.exists(sum_file):
         os.remove(sum_file)
 
     with open(sum_file, 'w') as outfile:
-        json.dump(all_streets_arr, outfile)
+        json.dump(street_data, outfile)
 
 if __name__ == '__main__':
     main()
